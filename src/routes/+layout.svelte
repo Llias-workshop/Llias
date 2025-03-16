5d79f3a3f4d15f58d91eeb58fe4928b4f83ef86e
Sanaz Alpr <s.alipour.m@gmail.com>
2025-03-16 21:04:58 +0330

    `Update svelte.config.js to remove config object and add prerender option`

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,5 @@
 <script>
-	import '../app.css';
+	import '../../static/app.css';
 	import Footer from '$lib/components/Footer.svelte';
 	import Navbar from '$lib/components/Navbar.svelte';
 </script>
---
+++ tailwind.config.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+    content: [
+      './src/**/*.{html,js,svelte,ts}', // بررسی تمام فایل‌ها در src
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  
