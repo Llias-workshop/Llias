96d12b6460cd7c106c754be50d963a9a4363bf92
Sanaz Alpr <s.alipour.m@gmail.com>
2025-03-16 15:03:06 +0330

    fix:

--- svelte.config.js
+++ svelte.config.js
@@ -2,12 +2,12 @@ import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		adapter: adapter(),
-		paths: {
-		    base: process.env.NODE_ENV === "production" ? "/Llias" : "",	
-		}
-	}
+  kit: {
+    adapter: adapter(),
+    paths: {
+      base: process.env.NODE_ENV === 'production' ? '/Llias' : '',  // فقط تنظیم base
+    },
+  },
 };
 
 export default config;
